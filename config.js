97f9191432d0d0614d57b4aac579919d7aa178fb
Natsunvt <96591770+Natsunvt@users.noreply.github.com>
2021-12-24 01:32:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,14 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    introTitle: 'Quỳnh ơi',
+    btnIntro: 'Bấm vào đây đi',
+    title: 'Ngày mai mày mua đồ ăn cho Thành nha',
+    desc: 'Trà sữa kem lăn rất cảm ơn bạn Quỳnh ',
+    btnYes: 'Ok mai tao bao m',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại tốt với mình như thế',
+    btnReply: 'Gửi cho bạn ❤',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messDesc: 'Bạn mang lên lúc 4 5h sáng mình vẫn nhận nha',
+    btnAccept: 'Okiiiii lun ❤',
